721cf7e67c9defb79ce19138ea9e7109a4547c1d
Furkan Karaoglu <karaoglufurkan@outlook.com>
2019-08-03 20:28:25 +0300

    dashboard added

--- blog.py
+++ blog.py
@@ -110,5 +110,9 @@ def logout():
     session.clear()
     return redirect(url_for("index"))
 
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
 if __name__ == "__main__":
     app.run("0.0.0.0", debug = True)
