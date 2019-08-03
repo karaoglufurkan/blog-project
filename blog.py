b7c7881899767a75b1bdde42e689a2123ff28a48
Furkan Karaoglu <karaoglufurkan@outlook.com>
2019-08-03 17:53:03 +0300

    registration and flash messages

--- blog.py
+++ blog.py
@@ -1,10 +1,10 @@
-from flask import Flask, flash,redirect, url_for, session, logging, request, render_template
-#if flask-mysqldb cannot be installed on macOSX, try first; 
-# $brew install mysql 
+from flask import Flask, flash, redirect, url_for, session, logging, request, render_template
+# if flask-mysqldb cannot be installed on macOSX, try first;
+#   $brew install mysql
 # if doesn't still work
-# $export PATH=$PATH:/usr/local/mysql/bin
-#then;
-# $pip install flask-mysqldb
+#   $export PATH=$PATH:/usr/local/mysql/bin
+# then;
+#   $pip install flask-mysqldb
 from flask_mysqldb import MySQL 
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
@@ -22,17 +22,18 @@ class RegisterForm(Form):
 
 
 app = Flask(__name__)
+app.secret_key = "fkblog"
 
+#mysql configurations
 app.config["MYSQL_HOST"] = "localhost"
+#app.config["MYSQL_PORT"] = "80"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = ""
 app.config["MYSQL_DB"] = "fkblog"
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
+#
 mysql = MySQL(app)
-
-
-
 
 @app.route("/")
 def index():
@@ -46,16 +47,34 @@ def about():
 def detail(id):
     return "Article ID: " + id
 
+#Kayıt ol sayfası
 @app.route("/register",methods = ["GET","POST"])
 def register():
     form = RegisterForm(request.form)
-    if request.method == "POST":
+    if request.method == "POST" and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(form.password.data)
+        #password = form.password.data
+
+
+        cursor = mysql.connection.cursor()
+        query = "insert into users(name, email, username, password) values(%s,%s,%s,%s)"
+        cursor.execute(query,(name, email, username, password))
+        mysql.connection.commit()
+        cursor.close()
+
+        flash(message = "Kayıt başarıyla tamamlandı!", category = "success")
+
         return redirect(location = url_for("index"))
     else:
         return render_template("register.html", form = form)
 
-    
-
+#Login sayfası
+@app.route("/login", methods = ["GET","POST"])
+def login():
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug = True)
